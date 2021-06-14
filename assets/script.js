61d817868afd612753e26961449a5bf473243303
Zahra Mertens <mertens.zahra@gmail.com>
2021-06-14 16:36:04 +1000

    changes

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lengthPassword;
+
+//Asign values to global variable which will be pushed to the password.
 var lowerCaseLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 //Convert lower case letters  upper case 
 var upper = lowerCaseLetter.map(lowerCaseLetter => lowerCaseLetter.toUpperCase());
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 var specialCharacters = ["~", "!", "@", "#", "$", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|"] 
-var options;
-var passwordEmpty = [];
-var userPassword = [];
-var userInput = "";
-var lowerCase;
-var upperCase;
-var number;
-var symbol;
+
 
 
 var generatePassword = function () {
 
+  //Define local variable only used in this function
+  var userPassword;
+  var userInput; 
+  var passwordEmpty = []; //Asign empty array to passwordEmpty that we can push the random selection of the userPassword variable inside.
+  
+  //Asking user how many characters they want their password to be.
   userInput = prompt("How long would you like your password to be? Choose from 8 to 128 characters!");
   
+  //If user clicks "cancel" the browser will tell them that the password length needs to be defined.
   if (!userInput) {
     alert("The password length needs to be defined!");
   
+    //If the number is below 8 or above 128, the browser will ask again for a user input between 8 and 128.
   } else if ( userInput < 8 || userInput > 128) {
     
-    userInput = prompt("The password length must be between 8 and 128 characters!");
+    var userInput = prompt("The password length must be between 8 and 128 characters!");
 
+    //If the user enters a number between 8 and 128 the browser will ask what characters the user want the password to contain.
   } else {
 
     var lowerCase = confirm("Would you like your password to include lower case characters?");
@@ -37,47 +40,17 @@ var generatePassword = function () {
   
   };
   
+  //Depending on the users answers all conditions below describe a different combination of characters.
+  
+  //If the user selects non of the character types the alert window will tell the user that at least one must be selected.
   if (!lowerCase && !upperCase && !number && !symbol) {
     
     alert("The password must contain one of the types!");
-
   } 
 
   //4 character types
   if (lowerCase && upperCase && number && symbol) {
     userPassword = lowerCaseLetter.concat(upper, numbers, specialCharacters);
-  
-    // 3 character types
-  } else if (!lowerCase && upperCase && number && symbol) {
-    userPassword = upper.concat(numbers, specialCharacters);
-  
-  } else if (lowerCase && !upperCase && number && symbol) {
-    userPassword = lowerCaseLetter.concat(numbers, specialCharacters);
-
-  } else if (lowerCase && upperCase && !number && symbol) {
-    userPassword = upper.concat(lowerCaseLetter, specialCharacters);
-  
-  } else if (lowerCase && upperCase && number && !symbol) {
-    userPassword = upper.concat(numbers, lowerCaseLetter);
-  
-  //2 character types
-  } else if (lowerCase && upperCase) {
-    userPassword = lowerCaseLetter.concat(upper);
-  
-  } else if (lowerCase && number) {
-    userPassword = lowerCaseLetter.concat(numbers);
-  
-  } else if (lowerCase && symbol) {
-    userPassword = lowerCaseLetter.concat(specialCharacters);
-  
-  } else if (upperCase && number) {
-    userPassword = upper.concat(numbers);
-  
-  } else if (number && symbol){
-    userPassword = numbers.concat(specialCharacters);
-  
-  } else if (upperCase && symbol) {
-    userPassword = upper.concat(specialCharacters);
   
   //Only 1 type of characters
   } else if (upperCase) {
@@ -91,7 +64,40 @@ var generatePassword = function () {
 
   } else if (number) {
     userPassword = numbers;
+  
+  //2 character types
+  } else if (lowerCase && upperCase && !number && !symbol) {
+    userPassword = lowerCaseLetter.concat(upper);
+  
+  } else if (lowerCase && number && !symbol && !upperCase) {
+    userPassword = lowerCaseLetter.concat(numbers);
+  
+  } else if (lowerCase && symbol && !upperCase && !number) {
+    userPassword = lowerCaseLetter.concat(specialCharacters);
+  
+  } else if (upperCase && number && !lowerCase && !symbol) {
+    userPassword = upper.concat(numbers);
+  
+  } else if (number && symbol && !upperCase && !lowerCase){
+    userPassword = numbers.concat(specialCharacters);
+  
+  } else if (upperCase && symbol && !number && !lowerCase) {
+    userPassword = upper.concat(specialCharacters);  
+    
+  // 3 character types
+  } else if (!lowerCase && upperCase && number && symbol) {
+    userPassword = upper.concat(numbers, specialCharacters);
+  
+  } else if (lowerCase && !upperCase && number && symbol) {
+    userPassword = lowerCaseLetter.concat(numbers, specialCharacters);
+
+  } else if (lowerCase && upperCase && !number && symbol) {
+    userPassword = upper.concat(lowerCaseLetter, specialCharacters);
+  
+  } else if (lowerCase && upperCase && number && !symbol) {
+    userPassword = upper.concat(numbers, lowerCaseLetter);
   };
+  
 
   for (var i = 0; i < userInput; i++ ) {
     
@@ -100,9 +106,8 @@ var generatePassword = function () {
   }
 
   var password = passwordEmpty.join('');
+  return password;
 }
-
-
 
 // Write password to the #password input
 function writePassword() {
